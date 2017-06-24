74cbb68835b3f43015fb56e72ff57beb02af35ca
Tim Pinkerton <timpinkerton@mail.com>
2017-06-24 15:06:11 -0400

    added middleware folder and updated layout

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,110 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var mid = require('../middleware');
+
+// GET /profile 
+router.get('/profile', mid.requiresLogin, function(req, res, next){
+
+    User.findById(req.session.userId)
+        .exec(function (error, user){
+          if (error) {
+            return next(error);
+          } else {
+            return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook});
+          }
+        });
+})
+
+//GET /logout
+router.get('/logout', function(req, res, next){
+  if (req.session) {
+    //delete the session object
+    req.session.destroy(function(err){
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    })
+  }
+});
+
+// GET /login
+router.get('/login', mid.loggedOut, function (req, res, next){
+  return res.render('login', { title: 'Log In' });
+});
+
+// POST /login
+router.post('/login', function(req, res, next){
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user){
+      if (error || !user) {
+        var err = new Error('Incorrect email of Password');
+        err.status = 401; 
+        return next(err);
+      } else {
+        req.session.userId = user._id; 
+        return res.redirect('/profile');
+      }
+    }); 
+  } else {
+    var err = new Error ('Email and Password are required!');
+    err.status = 401; 
+    return next(err); 
+  }
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, function(req, res, next){
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+
+  if (req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      // confirm that user typed the same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match!');
+        err.status = 400;
+        return next(err);   
+      }
+      
+        //all info is present and passwords match, create object with form input
+        //values are coming from the request object that the user filled out
+        //this is the new object that the document we will insert into mongo
+        var userData = {
+          email: req.body.email,
+          name: req.body.name, 
+          favoriteBook: req.body.favoriteBook,
+          password: req.body.password, 
+          confirmPassword: req.body.confirmPassword
+        };
+
+        //use schema's 'create' method to insert the document into mongo
+        //'User' is the mongoose model returned by the schema file
+        //'Create' is a mongoose method that inserts a new doc in mongo based on the model 
+        User.create(userData, function (error, user){
+          if (error) {
+            return next(error);
+          } else {
+            req.session.userId = user._id;
+            return res.redirect('/profile');
+          }
+        });
+    
+      } else {
+        var err = new Error('All fields required.');
+        err.status = 400;
+        return next(err);
+      }
+});
 
 // GET /
 router.get('/', function(req, res, next) {
